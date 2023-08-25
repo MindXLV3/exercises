31bd301b624e260659f85f183d73cc819e2856a4
Vo Tuong Vi <votuongvi2222002@gmail.com>
2023-08-25 20:29:14 +0700

    Ex1

---
+++ lesson-1/ex6.js
@@ -0,0 +1,24 @@
+/**
+ * a. 
+ * 
+ * Cho mảng gồm các số nguyên (ví dụ: [1, 6, 7, 22, 8]).
+ * Viết arow function sử dụng array function map() biến mảng đầu vào thành mảng mới với điều kiện:
+ *  - Phần tử chi hết cho 2 thì giữ nguyên.
+ *  - Phần tử không chia hết cho 2 thì nhân 2
+ * 
+ * Example:  [1, 6, 7, 22, 8] => [2, 12, 14, 22, 8]
+ */
+
+// Code here ...
+
+
+/**
+ * b.
+ * 
+ * Cho mảng gồm các số nguyên (ví dụ: [1, 6, 7, 22, 8]).
+ * Viết arow function sử dụng array function filter() và reduce() tính tổng các phần tử chi hết cho 2:
+ * 
+ * Example:  [1, 6, 7, 22, 8] => 36
+ */
+
+// Code here ...
